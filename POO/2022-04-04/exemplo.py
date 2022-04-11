2b1aa11ca09ce0750bf2de3d6b7b95bb9d66ede3
ppedraum <pedrovirtuozo@gmail.com>
2022-04-10 22:04:44 -0300

    trabalho de abstração
    exemplo feito

--- POO/2022-04-04/exemplo.py
+++ POO/2022-04-04/exemplo.py
@@ -1,16 +1,56 @@
 
-from abc import ABC, abstractclassmethod
+from abc import ABC, abstractmethod
+from re import X
 
-class Teste(ABC):
+class Matematica(ABC):
 
-    @abstractclassmethod
-    def metodoabs(self, x, y):
+    @abstractmethod
+    def multiplicar(self):
         pass
 
-class Main(Teste):
+    @abstractmethod
+    def somar(self):
+        pass
 
-    def metodoabs(self, x, y):
-        y=y*x
-        return super().metodoabs(x, y)
+class exemplo(Matematica):
 
-print(Main.metodoabs(1,2))
+    def __init__(self, x, y):
+        self.__x=x
+        self.__y=y
+
+    @property
+    def x(self):
+        return self.__x
+    @property
+    def y(self):
+        return self.__y
+
+    @x.setter
+    def x(self, valor):
+        print('Impossível alterar x!')
+    @x.setter
+    def y(self, valor):
+        print('Impossível alterar y!')
+
+    def somar(self):
+        return self.x+self.y
+
+    def multiplicar(self):
+        return self.x*self.y
+
+    def set_valores(self, x,y):
+        if x != None:
+            self.__x=x
+        if y != None:
+            self.__y=y
+
+ex=exemplo(1,2)
+print(ex.somar())
+print(ex.multiplicar())
+ex.set_valores(3,2)
+print(ex.somar())
+print(ex.multiplicar())
+
+ex.__x=1
+ex.__y=1
+print(ex.multiplicar())
